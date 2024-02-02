6e6032af78c906bf2d4b9d05a0fff3105c2eac69
Lakshya Mittal <lmittal@commvault.com>
2024-02-02 17:07:42 +0530

    Send Pong response

--- app/main.js
+++ app/main.js
@@ -3,13 +3,20 @@ const net = require("net");
 const PORT = 6379;
 const LOCALHOST = "127.0.0.1";
 
+function sendPongResponse(socket){
+    socket.write('+PONG\r\n');
+}
+
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
-const server = net.createServer((connection) => {
+const server = net.createServer((socket) => {
   // Handle connection
-  console.log('Client Connected');
+  socket.on('data', (data) => {
+    console.log(data);
+    sendPongResponse(socket);
+  });
 });
 
 server.listen(PORT, LOCALHOST, () => {
