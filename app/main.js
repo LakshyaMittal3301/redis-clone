427a6428778b1f9db72672256f91454a9e176480
Lakshya Mittal <lmittal@commvault.com>
2024-02-02 16:01:58 +0530

    Bind to port

--- app/main.js
+++ app/main.js
@@ -1,11 +1,17 @@
 const net = require("net");
 
+const PORT = 6379;
+const LOCALHOST = "127.0.0.1";
+
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
-// const server = net.createServer((connection) => {
-//   // Handle connection
-// });
-//
-// server.listen(6379, "127.0.0.1");
+const server = net.createServer((connection) => {
+  // Handle connection
+  console.log('Client Connected');
+});
+
+server.listen(PORT, LOCALHOST, () => {
+    console.log(`Server Listening on ${LOCALHOST}/${PORT}`);
+});
